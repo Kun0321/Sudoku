917cfc4b9d2878a4490212396262240e371672be
Kun0321 <126649055+Kun0321@users.noreply.github.com>
2023-04-16 13:47:27 +0800

    Update main.cpp

--- 數獨/main.cpp
+++ 數獨/main.cpp
@@ -1,8 +1,19 @@
-#include"Sudoku.h"
+#include <iostream>
+#include <vector>
+#include "Generate.h"
+#include "Answer.h"
 
-int main()
-{
-    Sudoku s;
-    s.generate();
+using namespace std;
+
+void print_board(vector <int >);
+
+int main(){
+    Generate NewBoard; // 產生棋盤物件
+    NewBoard.get_board();
+    
+    //Answer a;
+    //a.setTemp();
+
     return 0;
 }
+
