63a8b1675f09dfaa35751cf3bb0eab1d9fa493c4
Bob6541684 <101238821+Bob6541684@users.noreply.github.com>
2023-05-17 14:27:17 +0800

    Update main.cpp

--- 數獨/main.cpp
+++ 數獨/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include "Generate.h"
+#include "Blank.h"
+#include"FillBlank.h"
 #include "Answer.h"
 
 using namespace std;
@@ -8,12 +10,14 @@ using namespace std;
 void print_board(vector <int >);
 
 int main(){
+    int n, x, y, number;
+    cout<<"請輸入想要挖多少空格？";
+    cin>>n;
+    while(n<0 || n > 81){
+        cout<<"請重新輸入...";
+        cin>>n;
+    }
     Generate NewBoard; // 產生棋盤物件
-    NewBoard.get_board();
-    
-    //Answer a;
-    //a.setTemp();
-
-    return 0;
-}
-
+    Blank blank;
+    FillBlank fillblank;
+    Answer answer;}
