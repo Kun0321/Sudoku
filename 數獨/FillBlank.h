3186fe2d352eabc68b19c9e0759c6828e91e7e1f
shen3878753 <101238890+shen3878753@users.noreply.github.com>
2023-05-17 10:33:14 +0800

    Update FillBlank.h

--- 數獨/FillBlank.h
+++ 數獨/FillBlank.h
@@ -1,9 +1,13 @@
 #ifndef FILLBLANK_H
 #define FILLBLANK_H
 #include "Generate.h"
+#include<vector>
+
 class FillBlank{
 private:
-    int x,y,n;//x軸，y軸，填入的數字
+    vector<int > fill_board;
+    int x,y,number;//x軸，y軸，填入的數字
+
 public:
     FillBlank();//建構子
     FillBlank(int,int,int);//x軸，y軸，填入的數字
@@ -14,5 +18,11 @@ public:
     int getY() const;
     int getN() const;
 
+    void receive_board(vector <int >);//接收 Blank 已挖空的 board
+    vector<int > fill();//連入座標和數字
+    void get_board();//列印 board
+    vector<int> return_board();//回傳 board
+    
 };
+
 #endif
