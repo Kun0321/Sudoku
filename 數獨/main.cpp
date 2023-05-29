5662abe2d8864f0d3504bd7e1294fdfee2ac24e6
Kun0321 <126649055+Kun0321@users.noreply.github.com>
2023-05-29 11:50:15 +0800

    Update main.cpp

--- 數獨/main.cpp
+++ 數獨/main.cpp
@@ -10,6 +10,14 @@ using namespace std;
 void print_board(vector <int >);
 
 int main(){
+    Generate NewBoard; // 產生棋盤物件
+    Blank blank; 
+    FillBlank fillblank;
+    Answer answer;
+
+    blank.receive_board(NewBoard.get_board());
+    answer.receive_original_board(NewBoard.get_board());
+
     int n, x, y, number;
     cout<<"請輸入想要挖多少空格？";
     cin>>n;
@@ -17,7 +25,21 @@ int main(){
         cout<<"請重新輸入...";
         cin>>n;
     }
-    Generate NewBoard; // 產生棋盤物件
-    Blank blank;
-    FillBlank fillblank;
-    Answer answer;}
+    
+    blank.hole_board(n);
+    fillblank.receive_board(blank.return_board());
+    answer.receive_revise_board(fillblank.return_board());
+    fillblank.get_board();
+    
+    while(answer.comparison()){
+        cin>>x>>y>>number;
+        fillblank.setX(x);
+        fillblank.setY(y);
+        fillblank.setN(number);
+        answer.receive_revise_board(fillblank.fill());
+        answer.get_board();
+    }
+
+    return 0;
+}
+
