7c05303f905ca4912b9ab3cfc272ba5d53067cea
shen3878753 <101238890+shen3878753@users.noreply.github.com>
2023-06-04 17:23:30 +0800

    Update Generate.cpp

--- 數獨/Generate.cpp
+++ 數獨/Generate.cpp
@@ -52,7 +52,8 @@ void Generate::generator() {//亂數法產生新棋盤
 
 }
 
-vector <int > Generate::get_board(){ //輸出棋盤至vector變數
+vector <int > Generate::get_board() //輸出棋盤至vector變數
+{
     vector < int > out_board (SIZE*SIZE);
     for (i=0;i<SIZE;i++)
         for (j=0;j<SIZE;j++)
@@ -70,4 +71,73 @@ vector <int > Generate::get_board(){ //輸出棋盤至vector變數
     // cout << endl;
     
     return out_board;
+}
+
+bool Generate::check_rep(vector<int> board,int pos,int n)
+{
+    int i;
+    for (i=0;i<9;i++)
+    {
+        if(board[pos/9*9+i]==n) //check橫排上數字是否有重複
+        {
+            return true; //有重複，回傳true
+        }
+        if(board[i*9+pos%9]==n) //check直排上數字是否有重複
+        {
+            return true; //有重複，回傳true
+        }
+    }
+    return false;  //無重複，回傳false
+}
+
+bool Generate::board_put(vector<int > &board) //填入數字函式
+{
+    int pos;
+    int count1,count2; //計數器
+    bool check = true; //check數字是否重複
+
+    for (j=0;j<9;j++) //填入的數字1-9 (j+1的結果)
+    {
+        count2 = 0; //計算單一數字填入總共甞試錯誤次數
+        for (i=1;i<9;i++) //依序將數字填入其餘八個小九宮格(左上角已經產生)
+        {
+            pos = i/3*27+i%3*3; //每個小九宮格的起始index
+            count1 = 0; //計算單一數字填入甞試錯誤次數
+            while (check==true)
+            {
+                k = rand() % 9;
+                check = check_rep(board,pos+k/3*9+k%3,j+1);
+                ++count1;
+                ++count2;
+                if (board[pos+k/3*9+k%3]==0 && check==false) //數字沒有重複，成功填入結束while loop
+                {
+                    board[pos+k/3*9+k%3] = j+1;
+                    check = true;
+                    break;
+                }
+                else
+                {
+                    check = true;
+                }
+                if (count1 > 100) // 錯誤超過100次，數字重填
+                {
+                    for (l=0;l<81;l++)
+                    {
+                        if (board[l]==j+1)
+                        {
+                            board[l]=0;
+                        }
+                    }
+                    board[j/3*9+j%3]=j+1;
+                    i = 0;
+                    break;
+                }
+                if (count2 > 1000) // 錯誤超過1000次，回傳false並結束函式
+                {
+                    return false;
+                }
+            }
+        }
+    }
+    return true; //成功填滿棋盤，回傳true
 }
