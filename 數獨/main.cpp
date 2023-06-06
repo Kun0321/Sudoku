c613731c0f523a42e741f9619ab933c5102ef59e
Kun0321 <126649055+Kun0321@users.noreply.github.com>
2023-06-06 23:57:50 +0800

    Update main.cpp

--- 數獨/main.cpp
+++ 數獨/main.cpp
@@ -8,35 +8,34 @@ using namespace std;
 
 int main(){
     Generate NewBoard; // 產生棋盤物件
-    Blank blank; 
-    FillBlank fillblank;
-    Answer answer;
+    Blank blank; // 產生挖空物件
+    FillBlank fillblank; //產生填空物件
+    Answer answer; //產生答案物件
 
-    blank.receive_board(NewBoard.get_board());
-    answer.receive_original_board(NewBoard.get_board());
+    blank.receive_board(NewBoard.get_board()); // 將棋盤物件產生的棋盤複製到挖空物件裡
+    answer.receive_original_board(NewBoard.get_board()); // 將棋盤物件產生的棋盤複製到答案物件裡
 
     int n, x, y, number;
     cout<<"請輸入想要挖多少空格？";
-    cin>>n;
+    cin>>n; // 輸入數字要挖多少空格
     while(n<0 || n > 81){
         cout<<"請重新輸入...";
         cin>>n;
     }
     
-    blank.hole_board(n);
-    fillblank.receive_board(blank.return_board());
-    answer.receive_revise_board(fillblank.return_board());
-    fillblank.get_board();
+    blank.hole_board(n); // 將上面輸入的數字傳到挖空物件裡，並進行挖空
+    fillblank.receive_board(blank.return_board()); // 將挖好空格的棋盤傳到填空物件裡
+    answer.receive_revise_board(fillblank.return_board()); // 將未完成填空的棋盤傳到答案進行是否正確
+    fillblank.get_board(); // 列印未完成填空的棋盤
     
-    while(answer.comparison()){
-        cin>>x>>y>>number;
+    while(answer.comparison()){ // 判斷有有完成挖空
+        cin>>x>>y>>number; 
         fillblank.setX(x);
         fillblank.setY(y);
         fillblank.setN(number);
-        answer.receive_revise_board(fillblank.fill());
-        answer.get_board();
+        answer.receive_revise_board(fillblank.fill()); // 將未完成填空的棋盤傳到答案進行是否正確
+        answer.get_board(); // 列印棋盤
     }
 
     return 0;
 }
-
